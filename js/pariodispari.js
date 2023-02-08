d4a4e8101b519ac63391336c866b094523d1753d
Brody14 <stefania.spinas@gmail.com>
2023-02-08 18:30:42 +0100

    FEAT even or odd

---
+++ js/pariodispari.js
@@ -0,0 +1,48 @@
+// PARI O DISPARI
+//L’utente sceglie pari o dispari
+
+const userChoice = prompt("Scegli pari o dispari").toLowerCase();
+console.log(userChoice);
+
+//e inserisce un numero da 1 a 5.
+
+const userNumber = parseInt(prompt("Inerisci un numero da 1 a 5"));
+console.log(userNumber);
+
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+const pcNumber = randomNumber();
+console.log(pcNumber);
+
+//Sommiamo i due numeri
+
+const sum = userNumber + pcNumber;
+console.log(sum);
+
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//Comunichiamo all’utente chi ha vinto. (decidete voi come)
+
+if (
+	(evenOddCheck(sum) && userChoice === "pari") ||
+	(evenOddCheck(sum) === false && userChoice === "dispari")
+) {
+	toPrint(`<div>HAI VINTO!</div>`);
+} else {
+	toPrint(`<div>HAI PERSO!</div>`);
+}
+
+//FUNZIONI
+
+function randomNumber() {
+	const number = Math.floor(Math.random() * 5 + 1);
+	return number;
+}
+
+function evenOddCheck(total) {
+	let resto = total % 2;
+	return resto === 0;
+}
+
+function toPrint(print) {
+	document.getElementById("result").innerHTML += print;
+}
--- js/script.js
+++ js/script.js
@@ -1,61 +1,59 @@
-console.log('Justin Reyes')
+//console.log("Justin Reyes");
 
 //Palindroma
 
+// //Chiedere all’utente di inserire una parola
 
-//Chiedere all’utente di inserire una parola
-
-const userWord = prompt('Inserisci una parola').toLowerCase();
+const userWord = prompt("Inserisci una parola").toLowerCase();
 console.log(userWord);
 
 let wordList = [];
 
-for(let i = 0; i < userWord.length; i++) {
-    let letter = userWord[i];
-    wordList.push(letter);
-    console.log(wordList)
+for (let i = 0; i < userWord.length; i++) {
+	let letter = userWord[i];
+	wordList.push(letter);
+	console.log(wordList);
 }
 
 let wordReverseList = [];
 
-for(let i = wordList.length - 1; i >= 0; i--) {
-    wordReverseList.push(wordList[i])
-    console.log(wordReverseList)
+for (let i = wordList.length - 1; i >= 0; i--) {
+	wordReverseList.push(wordList[i]);
+	console.log(wordReverseList);
 }
 
-let wordString = '';
+let wordString = "";
 
-for(let i = 0; i < wordList.length; i++) {
-    let listToString = wordList[i];
-    wordString += listToString;
-    console.log(wordString)
+for (let i = 0; i < wordList.length; i++) {
+	let listToString = wordList[i];
+	wordString += listToString;
+	console.log(wordString);
 }
 
-let wordReverseString = '';
+let wordReverseString = "";
 
-for(let i = wordReverseList.length - 1; i >= 0; i--) {
-    let listToString = wordList[i];
-    wordReverseString += listToString;
-    console.log(wordReverseString)
+for (let i = wordReverseList.length - 1; i >= 0; i--) {
+	let listToString = wordList[i];
+	wordReverseString += listToString;
+	console.log(wordReverseString);
 }
 
-//Creare una funzione per capire se la parola inserita è palindroma
+// Creare una funzione per capire se la parola inserita è palindroma
 
-//Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
+// Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
 
-   if(printResult(userWord)){
-       toPrint(`<div>${userWord} è una parola palindroma!</div>`)
-   } else toPrint(`<div>${userWord} non è una parola palindroma!</div>`)
+if (printResult(userWord)) {
+	toPrint(`<div>${userWord} è una parola palindroma!</div>`);
+} else toPrint(`<div>${userWord} non è una parola palindroma!</div>`);
 
+//FUNZIONI
 
-  //FUNZIONI
+function toPrint(print) {
+	document.getElementById("result").innerHTML += print;
+}
 
-   function toPrint(print){
-       document.getElementById('result').innerHTML += print;
-   }
-
-   function printResult(word){
-       if(wordString === wordReverseString) {
-          return true
-       }
-   }
+function printResult(word) {
+	if (wordString === wordReverseString) {
+		return true;
+	}
+}

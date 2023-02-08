add2c60c028bd7e0e01eec68595488eb958da82f
Brody14 <stefania.spinas@gmail.com>
2023-02-08 17:35:28 +0100

    EDIT without reverse and join

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ console.log('Justin Reyes')
 const userWord = prompt('Inserisci una parola').toLowerCase();
 console.log(userWord);
 
-const wordList = [];
+let wordList = [];
 
 for(let i = 0; i < userWord.length; i++) {
     let letter = userWord[i];
@@ -16,40 +16,46 @@ for(let i = 0; i < userWord.length; i++) {
     console.log(wordList)
 }
 
-const wordReverseList = [...wordList].reverse();
-console.log(wordReverseList);
+let wordReverseList = [];
+
+for(let i = wordList.length - 1; i >= 0; i--) {
+    wordReverseList.push(wordList[i])
+    console.log(wordReverseList)
+}
+
+let wordString = '';
+
+for(let i = 0; i < wordList.length; i++) {
+    let listToString = wordList[i];
+    wordString += listToString;
+    console.log(wordString)
+}
+
+let wordReverseString = '';
+
+for(let i = wordReverseList.length - 1; i >= 0; i--) {
+    let listToString = wordList[i];
+    wordReverseString += listToString;
+    console.log(wordReverseString)
+}
 
 //Creare una funzione per capire se la parola inserita è palindroma
 
+//Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
 
- let wordListJoined = joined(wordList)
- console.log(wordListJoined);
-
- let wordReverseListJoined = joined(wordReverseList)
- console.log(wordReverseListJoined);
-
-// //Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
-
- if(printResult(userWord)){
-     toPrint(`<div>${userWord} è una parola palindroma!</div>`)
- } else toPrint(`<div>${userWord} non è una parola palindroma!</div>`)
+   if(printResult(userWord)){
+       toPrint(`<div>${userWord} è una parola palindroma!</div>`)
+   } else toPrint(`<div>${userWord} non è una parola palindroma!</div>`)
 
 
-// //FUNZIONI
+  //FUNZIONI
 
- function joined(list) {
-     let join = list.join()
-     return join
- }
+   function toPrint(print){
+       document.getElementById('result').innerHTML += print;
+   }
 
- function toPrint(print){
-     document.getElementById('result').innerHTML += print;
- }
-
- function printResult(word){
-
-     if(wordListJoined === wordReverseListJoined) {
-        return true
-     }
-
- }
+   function printResult(word){
+       if(wordString === wordReverseString) {
+          return true
+       }
+   }
